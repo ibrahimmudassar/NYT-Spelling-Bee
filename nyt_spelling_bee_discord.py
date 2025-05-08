dbeabbf81118105fbddf74942f7af4e5994d9bf9
Ibrahim Mudassar <22484328+ibrahimmudassar@users.noreply.github.com>
2025-05-08 14:31:10 -0400

    try except for thumbnail of valid letters

--- nyt_spelling_bee_discord.py
+++ nyt_spelling_bee_discord.py
@@ -1,6 +1,7 @@
 # An implementation of nyt_spelling_bee_solver.py but it sends a discord embed to be
 
-from PIL import Image
+import os
+from PIL import Image, UnidentifiedImageError
 import json
 from datetime import datetime
 from pprint import pprint
@@ -26,15 +27,16 @@ def ispangram(word, alphabet):
     return True
 
 
-soup = BeautifulSoup(requests.get(
-    "https://www.nytimes.com/puzzles/spelling-bee").content, "html.parser")
+soup = BeautifulSoup(
+    requests.get("https://www.nytimes.com/puzzles/spelling-bee").content, "html.parser"
+)
 key = soup.findAll("script", {"type": "text/javascript"})
 gameData = ""
 for i in key:
     if "window.gameData = " in i.text:
         gameData = i.text.replace("window.gameData = ", "")
 
-gameData = json.loads(gameData)['today']
+gameData = json.loads(gameData)["today"]
 # pprint(gameData)
 
 frequencies = {}
@@ -44,7 +46,7 @@ for word in gameData["answers"]:
     # The Zipf frequency of a word is the base-10 logarithm of the number of times it appears per billion words. A word with Zipf value 6 appears once per thousand words, for example, and a word with Zipf value 3 appears once per million words.
     # Reasonable Zipf values are between 0 and 8, but because of the cutoffs described above
     # 0 is the default Zipf value for words that do not appear in the given wordlist, although it should mean one occurrence per billion words
-    frequencies[word] = zipf_frequency(word, 'en')
+    frequencies[word] = zipf_frequency(word, "en")
 
     score = 0
 
@@ -53,20 +55,25 @@ for word in gameData["answers"]:
     else:
         score = len(word)
 
-    if ispangram(word=word, alphabet="".join(gameData['validLetters'])):
+    if ispangram(word=word, alphabet="".join(gameData["validLetters"])):
         score += 7
 
     graded[word] = score
 
 
-df = pd.DataFrame(frequencies.items(),
-                  columns=['word', 'freq (occurrence per billion)'])
-df['score'] = df['word'].map(graded)
-df["isPangram"] = [ispangram(i, "".join(
-    gameData['validLetters'])) for i in df['word']]
+df = pd.DataFrame(
+    frequencies.items(), columns=["word", "freq (occurrence per billion)"]
+)
+df["score"] = df["word"].map(graded)
+df["isPangram"] = [ispangram(i, "".join(gameData["validLetters"])) for i in df["word"]]
 
-fig = px.strip(df, x='freq (occurrence per billion)',
-               hover_data=df.columns, color="isPangram", title=f"NYT Spelling Bee Logarithmic Word Frequency Spread {gameData['printDate']}")
+fig = px.strip(
+    df,
+    x="freq (occurrence per billion)",
+    hover_data=df.columns,
+    color="isPangram",
+    title=f"NYT Spelling Bee Logarithmic Word Frequency Spread {gameData['printDate']}",
+)
 
 # Add image
 
@@ -83,81 +90,89 @@ fig = px.strip(df, x='freq (occurrence per billion)',
 fig.write_image("fig1.png", width=1280, height=720)
 
 # print the top 10 scores
-top_5 = df.nlargest(5, 'score').query('isPangram == False')["word"].tolist()
+top_5 = df.nlargest(5, "score").query("isPangram == False")["word"].tolist()
 
-rare_5 = df.nsmallest(5, 'freq (occurrence per billion)').query(
-    'isPangram == False')["word"].tolist()
+rare_5 = (
+    df.nsmallest(5, "freq (occurrence per billion)")
+    .query("isPangram == False")["word"]
+    .tolist()
+)
 
 
 # create embed object for webhook
-embed = DiscordEmbed(title='NYT Spelling Bee Answers', color='f7db1b')
+embed = DiscordEmbed(title="NYT Spelling Bee Answers", color="f7db1b")
 
 # set author
-embed.set_author(name=gameData['editor'])
+embed.set_author(name=gameData["editor"])
 
-embed.set_image(url='attachment://fig1.png')
+embed.set_image(url="attachment://fig1.png")
 
 # set thumbnail
 # Open the image from the URL
-now = datetime.now()
-image_url = f'https://static01.nyt.com/images/{now.strftime("%Y/%m/%d")}/multimedia/{now.strftime("%d")}morning-bee/{now.strftime("%d")}morning-bee-superJumbo.png?quality=100&auto=png'
-image = Image.open(requests.get(image_url, stream=True).raw)
+try:
+    now = datetime.now()
+    image_url = f'https://static01.nyt.com/images/{now.strftime("%Y/%m/%d")}/multimedia/{now.strftime("%d")}morning-bee/{now.strftime("%d")}morning-bee-superJumbo.png?quality=100&auto=png'
+    image = Image.open(requests.get(image_url, stream=True).raw)
 
-# Make the white background transparent
-image = image.convert("RGBA")
-datas = image.getdata()
-new_data = []
-for item in datas:
-    if item[0] == 255 and item[1] == 255 and item[2] == 255:
-        new_data.append((255, 255, 255, 0))
+    # Make the white background transparent
+    image = image.convert("RGBA")
+    datas = image.getdata()
+    new_data = []
+    for item in datas:
+        if item[0] == 255 and item[1] == 255 and item[2] == 255:
+            new_data.append((255, 255, 255, 0))
+        else:
+            new_data.append(item)
+    image.putdata(new_data)
+
+    # Center crop the image to 1:1 aspect ratio
+    width, height = image.size
+    if width > height:
+        left = (width - height) / 2
+        top = 0
+        right = (width + height) / 2
+        bottom = height
     else:
-        new_data.append(item)
-image.putdata(new_data)
+        left = 0
+        top = (height - width) / 2
+        right = width
+        bottom = (height + width) / 2
+    image = image.crop((left, top, right, bottom))
 
-# Center crop the image to 1:1 aspect ratio
-width, height = image.size
-if width > height:
-    left = (width - height) / 2
-    top = 0
-    right = (width + height) / 2
-    bottom = height
-else:
-    left = 0
-    top = (height - width) / 2
-    right = width
-    bottom = (height + width) / 2
-image = image.crop((left, top, right, bottom))
-
-# Save the resulting image as a PNG
-image.save("result.png", "PNG")
-embed.set_thumbnail(
-    url='attachment://result.png')
+    # Save the resulting image as a PNG
+    image.save("result.png", "PNG")
+    embed.set_thumbnail(url="attachment://result.png")
+except UnidentifiedImageError:
+    pass
 
 # set footer
-embed.set_footer(text='Made By Ibrahim Mudassar',
-                 icon_url='https://avatars.githubusercontent.com/u/22484328?v=4')
+embed.set_footer(
+    text="Made By Ibrahim Mudassar",
+    icon_url="https://avatars.githubusercontent.com/u/22484328?v=4",
+)
 
 # set timestamp (default is now) accepted types are int, float and datetime
 embed.set_timestamp()
 
 # add fields to embed
-embed.add_embed_field(name='Pangrams', value="\n".join(gameData["pangrams"]))
+embed.add_embed_field(name="Pangrams", value="\n".join(gameData["pangrams"]))
 
 # add fields to embed
-embed.add_embed_field(name='Top Words', value="\n".join(top_5))
+embed.add_embed_field(name="Top Words", value="\n".join(top_5))
 
 # add fields to embed
-embed.add_embed_field(name='Rare Words', value="\n".join(rare_5))
+embed.add_embed_field(name="Rare Words", value="\n".join(rare_5))
 
 # add embed object to webhook(s)
 for webhook_url in env.list("WEBHOOKS"):
     webhook = DiscordWebhook(url=webhook_url)
 
     with open("fig1.png", "rb") as f:
-        webhook.add_file(file=f.read(), filename='fig1.png')
-
-    with open("result.png", "rb") as f:
-        webhook.add_file(file=f.read(), filename='result.png')
+        webhook.add_file(file=f.read(), filename="fig1.png")
+    
+    if os.path.exists("result.png"):
+        with open("result.png", "rb") as f:
+            webhook.add_file(file=f.read(), filename="result.png")
 
     webhook.add_embed(embed)
     webhook.execute()
